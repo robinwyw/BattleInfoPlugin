f13c3023a1a41433f7413c0c5c34138790d4fe7a
lasedark <laserdark@outlook.com>
2020-01-05 15:25:21 +0800

    version updated

--- BattleInfoPlugin/Plugin.cs
+++ BattleInfoPlugin/Plugin.cs
@@ -15,8 +15,8 @@ namespace BattleInfoPlugin
     [ExportMetadata("Guid", "55F1599E-5FAD-4696-A972-BF8C4B3C1B76")]
     [ExportMetadata("Title", "BattleInfo")]
     [ExportMetadata("Description", "戦闘情報を表示します。")]
-    [ExportMetadata("Version", "2.0.0")]
-    [ExportMetadata("Author", "@veigr, @Madmanmayson")]
+    [ExportMetadata("Version", "2.1.0")]
+    [ExportMetadata("Author", "@veigr, @Madmanmayson, @laserdark")]
     public class Plugin : IPlugin, ITool, IRequestNotify, ILocalizable
     {
         private readonly ToolViewModel vm;
