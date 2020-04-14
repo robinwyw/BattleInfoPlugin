e57dea02272f5000057e85745d192765b0f458e3
lasedark <laserdark@outlook.com>
2020-04-14 19:31:08 +0800

    Add apis for anchorag_repair

---
+++ BattleInfoPlugin/Models/Raw/map_anchorage_repair.cs
@@ -0,0 +1,11 @@
+﻿using Grabacr07.KanColleWrapper.Models.Raw;
+
+namespace BattleInfoPlugin.Models.Raw
+{
+    public class map_anchorage_repair
+    {
+        public int[] api_used_ship { get; set; }
+        public int[] api_repair_ships { get; set; }
+        public kcsapi_ship2[] api_ship_data { get; set; }
+    }
+}
