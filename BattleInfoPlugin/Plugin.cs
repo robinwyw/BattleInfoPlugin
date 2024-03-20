80386d5da1a143d2263bd3899cf873d11e3d7013
robinwyw <wuweilogo@live.com>
2024-03-21 00:31:01 +0800

    update version

--- BattleInfoPlugin/Plugin.cs
+++ BattleInfoPlugin/Plugin.cs
@@ -15,7 +15,7 @@ namespace BattleInfoPlugin
     [ExportMetadata("Guid", "55F1599E-5FAD-4696-A972-BF8C4B3C1B76")]
     [ExportMetadata("Title", "BattleInfo")]
     [ExportMetadata("Description", "戦闘情報を表示します。")]
-    [ExportMetadata("Version", "2.5")]
+    [ExportMetadata("Version", "2.6.0")]
     [ExportMetadata("Author", "@veigr, @Madmanmayson, @laserdark")]
     public class Plugin : IPlugin, ITool, IRequestNotify, ILocalizable
     {
